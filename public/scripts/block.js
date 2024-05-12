1a3087834f6f5135e73e0ff176cc38951e4b537b
miku39 <miku39@onsei.fans>
2024-05-12 12:56:19 +0800

    [Block] Modify script

--- public/scripts/block.js
+++ public/scripts/block.js
@@ -1,5 +1,23 @@
 console.log('block');
 
-var main = document.getElementById('main_content');
-var parent = main.parentElement;
-parent.removeChild(main)
+const blockItems = ['AIP', 'RG60289'];
+
+const worksFilter = (work) => {
+    workAttr = work.getElementsByClassName('__product_attributes')[0]?.getAttribute('value');
+    if (!workAttr) return;
+    for (i of blockItems) {
+        if (workAttr.includes(i)) {
+            work.remove();
+            return;
+        }
+    }
+};
+
+const worksBlock = () => {
+    var works = document.getElementsByClassName('n_worklist')[0];
+    if (!works) return;
+    var worksArr = works.getElementsByTagName('tbody').length == 1 ? Array.from(works.children[0].children) : Array.from(works.children);
+    worksArr.forEach(worksFilter);
+};
+
+worksBlock();
